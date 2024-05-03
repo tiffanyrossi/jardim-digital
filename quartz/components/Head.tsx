fc9d83230e7f560f26d5276e6003315f59ab896f
Tiffany Rossi <tiffanyrossi@gmail.com>
2024-05-02 22:59:59 -0300

    Quartz sync: May 2, 2024, 10:59 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -20,7 +20,7 @@ export default (() => {
 
     return (
       <head>
-        <title>{title}</title>
+        <title>{title} | wiki.tiffs.dev 🧠</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
