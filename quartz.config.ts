a9bd4cefda950f155e719824051151e55b04cb4b
Tiffany Rossi <tiffanyrossi@gmail.com>
2024-05-04 00:32:19 -0300

    Quartz sync: May 4, 2024, 12:32 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Georgia",
-        body: "verdana",
+        body: "Tahoma",
         code: "Fira Code",
       },
       colors: {
