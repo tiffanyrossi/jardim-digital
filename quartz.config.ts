23f5605610564b49667d9ceb0a5d70c9e664cefe
Tiffany Rossi <tiffanyrossi@gmail.com>
2024-05-04 18:15:42 -0300

    Quartz sync: May 4, 2024, 6:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴🧠",
+    pageTitle: "🐝👒",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
